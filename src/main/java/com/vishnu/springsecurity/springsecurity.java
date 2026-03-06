6f589dfb1639377848e5635ae98e1f054b2a9d7b
Vishnu Choubey <vishnu.choubey@bounteous.com>
2026-03-06 09:18:09 +0530

    table naming inconsisitency fixed

--- src/main/java/com/vishnu/springsecurity/springsecurity.java
+++ src/main/java/com/vishnu/springsecurity/springsecurity.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringJwtApplication {
+public class springsecurity {
 	public static void main(String[] args) {
-        SpringApplication.run(SpringJwtApplication.class, args);
+        SpringApplication.run(springsecurity.class, args);
 	}
 }
